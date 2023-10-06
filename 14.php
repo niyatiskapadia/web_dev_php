555c717949d8b593e7710377a5cd6238f252fa6a
niyatiskapadia <144042740+niyatiskapadia@users.noreply.github.com>
2023-10-06 08:46:29 +0530

    Add files via upload
    14.Create a PHP program that uses echo to display the following text with proper formatting:

---
+++ 14.php
@@ -0,0 +1,11 @@
+<?php
+$unorder= "<ul>
+<li><font color='orange'> list </font></li><font color='pink'>1</font><br>
+<li><font color='orange'> list </font></li><font color='pink'>2</font><br>
+<li><font color='orange'> list </font></li><font color='pink'>3</font><br>
+
+</ul>";
+ 
+echo $unorder;  
+
+ ?>
