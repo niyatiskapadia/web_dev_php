f2b4cc6267537caf10890dfafea09c71770ac62b
niyatiskapadia <144042740+niyatiskapadia@users.noreply.github.com>
2023-10-06 08:45:19 +0530

    Add files via upload
    12.Develop a PHP program that uses echo to display an unordered list (<ul>) with three list items (<li>)
    containing different colors.

---
+++ 12.php
@@ -0,0 +1,10 @@
+<?php
+$unorder= "<ul>
+<li><font color='red'> list1 </font></li><br>
+<li><font color='orange'>list2</font></li><br>
+<li><font color='blue'>list3</font></li><br>
+</ul>";
+ 
+echo $unorder;  
+
+ ?>
