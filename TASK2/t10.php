d0e205c4bc3623b083ce5ccee440249d442a80ea
niyatiskapadia <144042740+niyatiskapadia@users.noreply.github.com>
2023-10-06 08:59:25 +0530

    Add files via upload
     Script_10: Write a PHP script to calculate the area of a rectangle given its length and width.

---
+++ TASK2/t10.php
@@ -0,0 +1,13 @@
+<?php
+$LENGTH=50;
+echo "THE LENGTH OF THE RECTANGLE IS " . $LENGTH . "</BR>";
+
+
+$WIDTH=40;
+
+echo "THE WIDTH OF THE RECTANGLE IS " . $WIDTH . "</BR>";
+
+
+echo "area of the rectangle is" . $LENGTH*$WIDTH;
+
+?>
